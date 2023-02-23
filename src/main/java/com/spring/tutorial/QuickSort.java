b5b712288941b199b4b8b97a55aae4a2a98bd005
Adnan525 <adnaan525@gmail.com>
2023-02-24 02:39:47 +1100

    primary bean

--- src/main/java/com/spring/tutorial/QuickSort.java
+++ src/main/java/com/spring/tutorial/QuickSort.java
@@ -1,7 +1,13 @@
 package com.spring.tutorial;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import java.util.Arrays;
 
+@Component
+@Primary
+//primary annotation to use this class as default if we have multiple classes of same implementation
 public class QuickSort implements SortingAlgorithm{
     @Override
     public int[] sort(int[] arr) {
